c3d63eb5a05d6d3af54b134b026738fc01094f96
Karcihan-Satheskishan-ICD2O1-01 <karcihan.satheskishan@student.tdsb.on.ca>
2024-09-17 13:57:11 +0000

    Sept 17 Work

--- PP1_3.py
+++ PP1_3.py
@@ -1,15 +1,30 @@
-
-
-def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-
+def q1():
+  print(" * * * * * *")
+  print("* * * * * * ")
+  print(" * * * * * *")
+  print("* * * * * * ")
+  print(" * * * * * *")
+  print("* * * * * * ")
+  print("\n* * * * *")
+  print("*       *")
+  print("*       *")
+  print("*       *")
+  print("* * * * *")
+  print("\n* * * * * *")
+  print(" *       *")
+  print("  *     *")
+  print("   *   *")
+  print("    * *")
+  print("     *")
+  
 def q2():
-  #Write Assignment code here
+  print("Hello", end=" ")
+  print("World")
   
 def q3():
-  #Write Assignment code here
-  
+  print('"Man"\n"Board"\n"Man Overboard"')
 
 
 #Do not alter anything after this line
